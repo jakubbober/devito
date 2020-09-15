ab4e5e0b91a54977b595bccfc4ad2743ed12f0a3
Fabio Luporini <f.luporini12@imperial.ac.uk>
2020-09-15 08:38:37 +0200

    gpu: Enable host-device movement for save!=None TimeFunctions [WIP]

--- devito/passes/iet/openmp.py
+++ devito/passes/iet/openmp.py
@@ -88,8 +88,7 @@ class OpenMPRegion(ParallelBlock):
 class OpenMPIteration(ParallelIteration):
 
     def __init__(self, *args, **kwargs):
-        kwargs.pop('pragmas', None)
-        pragma = self._make_header(**kwargs)
+        pragmas, kwargs = self._make_header(**kwargs)
 
         properties = as_tuple(kwargs.pop('properties', None))
         properties += (COLLAPSED(kwargs.get('ncollapse', 1)),)
@@ -101,17 +100,18 @@ class OpenMPIteration(ParallelIteration):
         self.nthreads = kwargs.pop('nthreads', None)
         self.reduction = kwargs.pop('reduction', None)
 
-        super(OpenMPIteration, self).__init__(*args, pragmas=[pragma],
+        super(OpenMPIteration, self).__init__(*args, pragmas=pragmas,
                                               properties=properties, **kwargs)
 
     @classmethod
     def _make_header(cls, **kwargs):
+        kwargs.pop('pragmas', None)
+
         construct = cls._make_construct(**kwargs)
         clauses = cls._make_clauses(**kwargs)
+        header = c.Pragma(' '.join([construct] + clauses))
 
-        header = ' '.join([construct] + clauses)
-
-        return c.Pragma(header)
+        return (header,), kwargs
 
     @classmethod
     def _make_construct(cls, parallel=False, **kwargs):
@@ -139,6 +139,11 @@ class OpenMPIteration(ParallelIteration):
             clauses.append('reduction(+:%s)' % args)
 
         return clauses
+
+    @classmethod
+    def _make_other_pragmas(cls, **kwargs):
+        # Subclass hook
+        return ()
 
 
 class ParallelTree(List):
